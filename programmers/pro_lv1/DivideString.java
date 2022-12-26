ef24812820cf5c0200fa38e07de02b356d3827d2
오윤성 <gagizzang92@gmail.com>
2022-12-26 14:06:11 +0900

    [20221226][가장가까운같은글자]풀이성공

--- programmers/pro_lv1/DivideString.java
+++ programmers/pro_lv1/DivideString.java
@@ -3,7 +3,26 @@ package pro_lv1;
 public class DivideString {
 
 	public static void main(String[] args) {
-
+		String s = "abracadabra";
+		solution(s);
 	}
-
+	
+	static int solution(String s) {
+		int len = s.length();
+		int answer = 0;
+		int num = 0;
+		char x = 0;
+		for(int i = 0 ; i < len ; i++) {
+			if(num == 0) {
+				answer++;
+				x = s.charAt(i);
+			}
+			if(x == s.charAt(i)) {
+				num++;
+			}else {
+				num--;
+			}
+		}
+		return answer;
+	}
 }
